c64bc597379988110c383cb2ac3cdfaf35ca7e4f
Vili Kinnunen <vili.kinnunen1@gmail.com>
2018-05-06 12:39:40 +0300

    Enable debugging Chrome

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./demo/index.html",
-  filename: "./index.html"
+  template: './demo/index.html',
+  filename: './index.html',
 });
 
 module.exports = {
-    entry: './demo/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    },
-    plugins: [htmlPlugin]
+  devtool: 'cheap-module-source-map',
+  entry: './demo/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
 };
