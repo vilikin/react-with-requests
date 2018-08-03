3f484e10a07b0e9fb86918b28ca96d1bd0f725df
Vili Kinnunen <vili.kinnunen1@gmail.com>
2018-08-03 12:10:40 +0300

    Improve export syntax of RequestStateContext

--- src/RequestStateContext.js
+++ src/RequestStateContext.js
@@ -2,12 +2,10 @@ import React from 'react'; // eslint-disable-line
 
 let requestStateContext;
 
-function getRequestStateContext() {
+export function getRequestStateContext() {
   if (requestStateContext) return requestStateContext;
 
   requestStateContext = React.createContext({});
 
   return requestStateContext;
 }
-
-export { getRequestStateContext };
